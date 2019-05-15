bdadee39267d2a9e26243367cd2d4ff8f2cd7e22
Manojkiran <manojkiran.dhurai@gmail.com>
2019-05-15 07:31:21 +0530

    fixed user model issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,20 +1,13 @@
 <?php
 
 namespace App\Models;
-
 use App\Models\BaseModel;
+use Illuminate\Auth\{MustVerifyEmail as MustVerifyEmailTrait, Authenticatable};
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-
-use Illuminate\Auth\{
-    MustVerifyEmail as MustVerifyEmailTrait,
-    Authenticatable
-};
-use Illuminate\Contracts\Auth\{
-    Authenticatable as AuthenticatableContract,
-    Authorizable as AuthorizableContract,
-    CanResetPassword as CanResetPasswordContract
-};
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends BaseModel implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
