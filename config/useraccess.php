1956c0682b1d9d491ffeef51cf72060ae94adbcc
Manojkiran <manojkiran.dhurai@gmail.com>
2019-05-25 10:38:57 +0530

    added config

--- config/useraccess.php
+++ config/useraccess.php
@@ -12,8 +12,47 @@ return [
     |
     */
 
-    
-    'permissionTable' => 'permissions',
-];
-    
+    /*
+    * When using the Roles we need to know which 
+    * table should be used to retrieve your roles. We have chosen a basic
+    * default value but you may easily change it to any table you like.
+    */
 
+    
+    'rolesTable' => 'roles',
+
+    /*
+    * When using the Permissions we need to know which 
+    * table should be used to retrieve your roles. We have chosen a basic
+    * default value but you may easily change it to any table you like.
+    */
+
+    'permissionsTable' => 'permissions',
+
+    /*
+    * When using the users has permissions , we need to know which
+    * table should be used to retrieve users permissions. We have chosen a
+    * basic default value but you may easily change it to any table you like.
+    */
+
+    'userPermissionsTable' => 'permission_user',
+
+    /*
+    * When using the users has roles, we need to know which
+    * table should be used to retrieve your user roles. We have chosen a
+    * basic default value but you may easily change it to any table you like.
+    */
+
+    'userRolesTable' => 'role_user',
+
+    /*
+    * When using the permission has roles, we need to know which
+    * table should be used to retrieve your role permissions. We have chosen a
+    * basic default value but you may easily change it to any table you like.
+    */
+
+    'rolePermissionsTable' => 'permission_role',
+
+    // DONT TOUCH AFTER THIS PART iy you dont know what is indented for
+
+];
