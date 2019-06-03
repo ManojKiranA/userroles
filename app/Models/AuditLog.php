47f8ff48017a2adc1432447d26d59ad368f5a1b7
Manojkiran <manojkiran.dhurai@gmail.com>
2019-06-03 09:58:46 +0530

    added filed sets to auditlogs

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuditLog extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['description','subject_id','subject_type','user_id','properties','host'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['properties' => 'collection'];
 }
